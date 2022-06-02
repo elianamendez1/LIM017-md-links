f2105795b1ae389c0f71659768692a83eaefed0a
elianamendez1 <elianamendez1809@gmail.com>
2022-06-02 15:04:31 -0500

    Task completed. Close #1

--- src/mdFunctions.js
+++ src/mdFunctions.js
@@ -1,27 +1,7 @@
+/* eslint-disable import/prefer-default-export */
 import fs from 'fs';
-import path from 'path';
+// import path from 'path';
 
-// Verifica si la ruta existe
+// Verificación de la existencia de la ruta
 export const isValidatedPath = (directory) => fs.existsSync(directory);
-
-// Verificamos si la ruta es absoluta
-export const convertPathAbsolute = (url) => (!path.isAbsolute(url) ? path.resolve(url) : url);
-console.log(isValidatedPath, convertPathAbsolute, 'lo convirtió???');
-
-/* // fs.readFileSync : Metodo Síncrono que se encarga de leer el contenido de un archivo específico
-export const readFile = (file) => fs.readFileSync(file, 'utf-8');
-console.log(readFile);
-
-// Verificamos si la ruta es un archivo
-export const pathIsFile = (route) => {
-  const statsObj = fs.lstatSync(route);
-  return statsObj.isFile();
-};
-
-// Verificamos si la ruta es una carpeta
-export const pathIsDirectory = (route) => {
-  const statsObj = fs.lstatSync(route);
-  return statsObj.isDirectory();
-};
-console.log(pathIsDirectory);
- */
+console.log('¿La ruta existe?', isValidatedPath('filesTest'));
--- src/mdLinks.js
+++ src/mdLinks.js
@@ -1,11 +1,1 @@
-import { convertPathAbsolute, isValidatedPath } from './mdFunctions';
-
-// ! -------------------------------------------------------------------------------
-
-export const mdLinks = (path, options) => new Promise((resolve, reject) => {
-  if (!isValidatedPath(path)) {
-    reject('La ruta ingresada no existe');
-  } else {
-    const AbsolutePath = convertPathAbsolute(path);
-  }
-});
+/* eslint-disable import/prefer-default-export */
