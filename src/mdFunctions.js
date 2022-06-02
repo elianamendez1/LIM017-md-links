e1e8ad07fa8d3b0752c580cc81a29e4d74127d93
elianamendez1 <elianamendez1809@gmail.com>
2022-06-02 16:33:39 -0500

    Add function directory, task completed. Close #6

--- src/mdFunctions.js
+++ src/mdFunctions.js
@@ -2,9 +2,16 @@ import fs from 'fs';
 import path from 'path';
 
 // Verificamos la existencia de la ruta
-export const isValidatedPath = (directory) => fs.existsSync(directory);
-console.log('¿La ruta existe?', isValidatedPath('filesTest'));
+export const validatePath = (directory) => fs.existsSync(directory);
+console.log('¿La ruta existe?', validatePath('filesTest'));
 
 // Si la ruta es relativa la convertimos a absoluta
 export const convertPathAbsolute = (url) => (!path.isAbsolute(url) ? path.resolve(url) : url);
 console.log('La ruta es relativa ahora la convertimos:', convertPathAbsolute('filesTest'));
+
+// Comprobamos si la ruta es una carpeta
+export const pathDirectory = (route) => {
+  const statsObj = fs.lstatSync(route);
+  return statsObj.isDirectory();
+};
+console.log('¿La ruta es un directorio?', pathDirectory('filesTest'));
