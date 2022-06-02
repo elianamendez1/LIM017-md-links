d3e93963164e764f51c39b6566b5ae6b76842e78
elianamendez1 <elianamendez1809@gmail.com>
2022-06-02 15:27:43 -0500

    change name function. Close #2 #3

--- src/mdFunctions.js
+++ src/mdFunctions.js
@@ -1,10 +1,10 @@
 import fs from 'fs';
 import path from 'path';
 
-// Verificación de la existencia de la ruta
+// Verificamos la existencia de la ruta
 export const isValidatedPath = (directory) => fs.existsSync(directory);
 console.log('¿La ruta existe?', isValidatedPath('filesTest'));
 
-// Si la ruta es si es relativa la convertimos a absoluta
+// Si la ruta es relativa la convertimos a absoluta
 export const convertPathAbsolute = (url) => (!path.isAbsolute(url) ? path.resolve(url) : url);
 console.log('La ruta es relativa ahora la convertimos:', convertPathAbsolute('filesTest'));
