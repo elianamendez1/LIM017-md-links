ec7baa6b9348e3bdbe08ebc0c6250e197ed166d0
elianamendez1 <elianamendez1809@gmail.com>
2022-06-02 15:17:42 -0500

    Task completed. Close #2 #3

--- src/mdFunctions.js
+++ src/mdFunctions.js
@@ -1,7 +1,10 @@
-/* eslint-disable import/prefer-default-export */
 import fs from 'fs';
-// import path from 'path';
+import path from 'path';
 
 // Verificación de la existencia de la ruta
 export const isValidatedPath = (directory) => fs.existsSync(directory);
 console.log('¿La ruta existe?', isValidatedPath('filesTest'));
+
+// Si la ruta es si es relativa la convertimos a absoluta
+export const convertPathAbsolute = (url) => (!path.isAbsolute(url) ? path.resolve(url) : url);
+console.log('La ruta es relativa ahora la convertimos:', convertPathAbsolute('filesTest'));
