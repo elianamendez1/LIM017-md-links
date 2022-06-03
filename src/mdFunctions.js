2df9532e41080373d98be9175af9ae22b281bd19
elianamendez1 <elianamendez1809@gmail.com>
2022-06-03 17:40:49 -0500

    Add function mdLinks. Close #16

--- src/mdFunctions.js
+++ src/mdFunctions.js
@@ -4,35 +4,35 @@ import fetch from 'node-fetch';
 
 // Verificamos la existencia de la ruta
 export const validatePath = (directory) => fs.existsSync(directory);
-console.log('¿La ruta existe?', validatePath('filesTest'));
+// console.log('¿La ruta existe?', validatePath('filesTest'));
 
 // Si la ruta es relativa la convertimos a absoluta
 export const convertAbsolutePath = (url) => (!path.isAbsolute(url) ? path.resolve(url) : url);
-console.log('La ruta es relativa ahora la convertimos:', convertAbsolutePath('filesTest'));
+// console.log('La ruta es relativa ahora la convertimos:', convertAbsolutePath('filesTest'));
 
 // Comprobamos si la ruta es una carpeta
 export const pathDirectory = (route) => {
   const statsObj = fs.lstatSync(route);
   return statsObj.isDirectory();
 };
-console.log('¿La ruta es un directorio?', pathDirectory('filesTest'));
+// console.log('¿La ruta es un directorio?', pathDirectory('filesTest'));
 
 // Comprobamos si la ruta es un archivo
 export const pathFile = (route) => {
   const statsObj = fs.lstatSync(route);
   return statsObj.isFile();
 };
-console.log('¿La ruta es un archivo?', pathFile('filesTest'));
+// console.log('¿La ruta es un archivo?', pathFile('filesTest'));
 
 // Leemos el contenido de archivos específicos
 export const readFile = (file) => fs.readFileSync(file, 'utf-8');
-console.log('leeeeee un archivoooo');
+// console.log('leeeeee un archivoooo');
 
 export const getFilesMd = (files) => {
   const mdFiles = files.filter((file) => path.extname(file) === '.md');
   return mdFiles.length > 0 ? mdFiles : [];
 };
-console.log(getFilesMd, 'funcioooon de traer archivos');
+// console.log(getFilesMd, 'funcioooon de traer archivos');
 
 export const recursionToGetFiles = (route) => {
   const filesArr = [];
@@ -46,7 +46,7 @@ export const recursionToGetFiles = (route) => {
   });
   return filesArr.flat();
 };
-console.log(recursionToGetFiles('filesTest'), 'recurrrsioooon');
+// console.log(recursionToGetFiles('filesTest'), 'recurrrsioooon');
 
 export const getAllLinks = (track) => {
   const pathAbsolute = convertAbsolutePath(track);
@@ -66,7 +66,7 @@ export const getAllLinks = (track) => {
   }
   return links;
 };
-console.log(getAllLinks('filesTest/test.md'));
+// console.log(getAllLinks('filesTest/test.md'));
 
 export const validatedLink = (link) => new Promise((resolve) => {
   const paramLink = link;
@@ -78,18 +78,18 @@ export const validatedLink = (link) => new Promise((resolve) => {
     })
     .catch(() => {
       paramLink.statusCode = 404;
+      console.log(paramLink.statusCode, 'soooooy eerrorrr 404');
       paramLink.message = 'Fail';
       resolve(link);
     });
 });
-// console.log(validatedLink('https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/map'));
+// console.log(validatedLink('test.md'));
 
 export const validatedLinks = (links) => new Promise((resolve) => {
   // es un array de promesas
   const newLinks = [];
   links.forEach((link) => newLinks.push(validatedLink(link)));
-  // eslint-disable-next-line no-promise-executor-return
-  return Promise.all(newLinks)
+  Promise.all(newLinks)
     .then((res) => resolve(res))
     .catch((err) => err);
 });
--- src/mdLinks.js
+++ src/mdLinks.js
@@ -1,1 +1,52 @@
-/* eslint-disable import/prefer-default-export */
+/* eslint-disable import/extensions */
+import {
+  validatePath,
+  convertAbsolutePath,
+  recursionToGetFiles,
+  getFilesMd,
+  validatedLinks,
+  getAllLinks,
+} from './mdFunctions.js';
+
+// ! -------------------------------------------------------------------------------
+
+const mdLinks = (path, options) => new Promise((resolve, reject) => {
+  if (!validatePath(path)) {
+    reject(new Error('La ruta ingresada no existe'));
+  } else {
+    const AbsolutePath = convertAbsolutePath(path);
+
+    const allFiles = recursionToGetFiles(AbsolutePath);
+
+    if (allFiles.length === 0) {
+      reject(new Error('No existe ningún archivo en la ruta ingresada'));
+    } else {
+      const mdFiles = getFilesMd(allFiles);
+      // console.log("allFiles", allFiles);
+
+      // console.log("miraaaaa mis MDFILES , ", mdFiles);
+      if (mdFiles.length === 0) {
+        reject(new Error('No existen archivos MD en la ruta ingresada'));
+      } else {
+        // console.log("mdFiles", mdFiles);
+
+        const links = [];
+        mdFiles.forEach((file) => {
+          links.push(...getAllLinks(file));
+        });
+
+        if (links.length === 0) {
+          reject(new Error('No existen links en la ruta ingresada'));
+        } else if (options.validate) {
+          resolve(validatedLinks(links));
+        } else {
+          resolve(links);
+        }
+      }
+    }
+  }
+});
+
+export default mdLinks;
+
+console.log(mdLinks('filesTest'));
