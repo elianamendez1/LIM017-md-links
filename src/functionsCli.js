b25af2815439ef2786fa463f8e2081312aa0e87e
elianamendez1 <elianamendez1809@gmail.com>
2022-06-06 11:00:13 -0500

    Add function brokenLinks

--- src/functionsCli.js
+++ src/functionsCli.js
@@ -1,1 +1,5 @@
 export const totalLinks = (links) => links.length;
+
+export const uniqueLinks = (links) => new Set(links.map((link) => link.href)).size;
+
+export const brokenLinks = (links) => links.filter((link) => link.statusCode === 404).length;
