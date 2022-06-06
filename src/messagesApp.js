3ae3cd60262d147ed97f92262e322e653e4fa7e5
elianamendez1 <elianamendez1809@gmail.com>
2022-06-06 11:45:16 -0500

    Add console messages. Close #21

---
+++ src/messagesApp.js
@@ -0,0 +1,197 @@
+/* eslint-disable import/extensions */
+import CFonts from 'cfonts';
+import gradient from 'gradient-string';
+import { table } from 'table';
+import chalk from 'chalk';
+import { totalLinks, uniqueLinks, brokenLinks } from './functionsCli.js';
+
+export const welcome = () => {
+  const line = '═══════════════════════════════════════════════════════════════════════════ \n';
+  const coolGradient = gradient(['#58167D', '#336535', '#B20053', '#850F5B']);
+
+  console.log(coolGradient(line));
+  // La marca
+  CFonts.say('md-links', {
+    font: 'block', // define the font face
+    align: 'left', // define text alignment
+    colors: ['system'], // define all colors
+    letterSpacing: 1, // define letter spacing
+    lineHeight: 0, // define the line height
+    space: false, // define if the output text should have empty lines on top and on the bottom
+    maxLength: '0', // define how many character can be on one line
+    gradient: ['#58167D', '#336535', '#B20053', '#850F5B'], // define your gradient colors
+    independentGradient: false, // define if you want to recalculate the gradient for each new line
+    transitionGradient: true, // define if this is a transition between colors directly
+    env: 'node', // define the environment CFonts is being executed in
+  });
+
+  // El eslogan
+  CFonts.say('  tool checker', {
+    font: 'chrome',
+    align: 'left',
+    colors: ['system'],
+    letterSpacing: 1,
+    lineHeight: 1,
+    space: false,
+    maxLength: '0',
+    length: '2',
+    gradient: ['#58167D', '#336535', '#B20053', '#850F5B'],
+    independentGradient: false,
+    transitionGradient: true,
+    env: 'node',
+  });
+
+  console.log(coolGradient(line));
+};
+
+export const help = () => {
+  console.log(`
+      "Usage: md-links <path> [option]"
+
+      Note: You can also use the alias command.           i.e. md-links -h
+                                                               md-links -v
+                                                               md-links ./some/example.md -s
+                                                               md-links ./some/example.md -va
+                                                               md-links ./some/example.md -s -va
+    `);
+
+  const data = [
+    [
+      `${chalk.hex('#FF8800').bold('OPTIONS')}`,
+      `${chalk.hex('#FF8800').bold('ALIAS')}`,
+      `${chalk.hex('#FF8800').bold('DESCRIPTION')}`,
+      `${chalk.hex('#FF8800').bold('EXAMPLE')}`,
+    ],
+    ['--help', '-h', 'Use to display this help', 'md-links --help'],
+    [
+      '--version',
+      '-v',
+      'Use to display the version of this\nmd-links package',
+      'md-links --version',
+    ],
+    [
+      '--validate',
+      '-va',
+      'Use to display a list of extra link\ninformation (StatusCode, and Ok or\nFail)',
+      'md-links ./some/example.md --validate',
+    ],
+    [
+      '--stats',
+      '-s',
+      'Use to display statistical link\ninformation (Total links and Unique\nlinks)',
+      'md-links ./some/example.md --stats',
+    ],
+    [
+      '--stats --validate',
+      '-s -va',
+      'Use to display statistical link\ninformation (Total links, Unique\nlinks and Broken Liks)',
+      'md-links ./some/example.md --stats\n--validate',
+    ],
+    [
+      'Empty option',
+      '',
+      'Use to display a list of basic link\ninformation',
+      'md-links ./some/example.md',
+    ],
+  ];
+  console.log(data);
+};
+
+export const statsValidate = (links) => {
+  const data = [
+    [`${chalk.hex('#FF8800').bold('Total Links')}`, `${totalLinks(links)}`],
+    [`${chalk.hex('#FF8800').bold('Unique Links')}`, `${uniqueLinks(links)}`],
+    [`${chalk.hex('#A41421').bold('Broken Links')}`, `${brokenLinks(links)}`],
+  ];
+
+  console.log('\nOptions selected: --stats and --validate\n');
+
+  const config = {
+    columns: {
+      0: { width: 15 },
+      1: { width: 2 },
+    },
+  };
+  console.log(table(data, config));
+  console.log('\n');
+};
+
+export const stats = (links) => {
+  const data = [
+    [`${chalk.hex('#FF8800').bold('Total Links')}`, `${totalLinks(links)}`],
+    [`${chalk.hex('#FF8800').bold('Unique Links')}`, `${uniqueLinks(links)}`],
+  ];
+
+  console.log('\nOption selected: --stats\n');
+
+  const config = {
+    columns: {
+      0: { width: 15 },
+      1: { width: 2 },
+    },
+  };
+  console.log(table(data, config));
+  console.log('\n');
+};
+
+export const tableLinksValidated = (links) => {
+  console.log('\nOption selected: --validate\n');
+
+  // const lista = [];
+
+  const prueba = links.map((link) => [
+    link.text.slice(0, 51),
+    link.href,
+    link.file,
+    link.statusCode,
+    link.message,
+  ]);
+
+  prueba.unshift([
+    `${chalk.hex('#FF8800').bold('Text')}`,
+    `${chalk.hex('#FF8800').bold('URL')}`,
+    `${chalk.hex('#FF8800').bold("File's path where link\nwas found")}`,
+    `${chalk.hex('#FF8800').bold('Status\nCode')}`,
+    `${chalk.hex('#FF8800').bold('Message')}`,
+  ]);
+
+  const config = {
+    columns: {
+      0: { width: 35 },
+      1: { width: 30 },
+      2: { width: 25 },
+      3: { width: 8 },
+      4: { width: 8 },
+    },
+  };
+  console.log(table(prueba, config));
+  console.log('\n');
+};
+
+export const tableLinks = (links) => {
+  console.log('\nOption selected: NONE\n');
+  // const lista = [];
+
+  const test = links.map((link) => [link.text.slice(0, 51), link.href, link.file]);
+
+  test.unshift([
+    `${chalk.hex('#FF8800').bold('Text')}`,
+    `${chalk.hex('#FF8800').bold('URL')}`,
+    `${chalk.hex('#FF8800').bold("File's path where link\nwas found")}`,
+  ]);
+
+  const config = {
+    columns: {
+      0: { width: 35 },
+      1: { width: 35 },
+      2: { width: 35 },
+    },
+  };
+  console.log(table(test, config));
+  console.log('\n');
+};
+
+// Para lanzar mensajes de salida con formato de letras amarillas
+export const outputMessage = (str) => `${chalk.yellow(str)}`;
+// Para lanzar mensajes de error con formato de letras rojas
+export const errorMessage = (str) => `${chalk.redBright(str)}`;
